57bb7316a15959dafa2558a20404ba4813945b6d
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-06-26 16:19:27 -0400

    Pages and routes skeleton created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
+
 const App = () => {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />}></Route>
+          <Route path='/offers' element={<Offers />}></Route>
+          <Route path='/profile' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/forgot-password' element={<ForgotPassword />}></Route>
+        </Routes>
+        {/* Navbar goes here */}
+      </Router>
     </>
   )
 }
