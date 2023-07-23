6d0b832be5784e616db2399d76b4b41e5a15a6ec
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-07-23 17:44:30 -0400

    Load more pagination

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -7,6 +7,7 @@ import {
   where,
   orderBy,
   limit,
+  startAfter,
 } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
@@ -16,6 +17,7 @@ import ListingItem from '../components/ListingItem'
 const Category = () => {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
   const params = useParams()
 
@@ -36,7 +38,11 @@ const Category = () => {
         //Execute query
         const querySnap = await getDocs(q)
 
-        let listings = []
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+
+        setLastFetchedListing(lastVisible)
+
+        const listings = []
 
         querySnap.forEach((doc) => {
           return listings.push({
@@ -51,18 +57,57 @@ const Category = () => {
         toast.error('Could not fetch listings')
       }
     }
+
     fetchListings()
   }, [params.categoryName])
+
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings')
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where('type', '==', params.categoryName),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      )
+
+      // Execute query
+      const querySnap = await getDocs(q)
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+      setLastFetchedListing(lastVisible)
+
+      const listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings((prevState) => [...prevState, ...listings])
+      setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch listings')
+    }
+  }
 
   return (
     <div className='category'>
       <header>
         <p className='pageHeader'>
           {params.categoryName === 'rent'
-            ? 'Palces for Rent'
+            ? 'Places for Rent'
             : 'Places for Sale'}
         </p>
       </header>
+
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
@@ -71,13 +116,20 @@ const Category = () => {
             <ul className='categoryListings'>
               {listings.map((listing) => (
                 <ListingItem
-                  key={listing.id}
                   listingItem={listing.data}
                   id={listing.id}
+                  key={listing.id}
                 />
               ))}
             </ul>
           </main>
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className='loadMore' onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>No listings for {params.categoryName}</p>
